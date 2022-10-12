a96dc0448614fd69c070d3958659eba9a9ad2d4a
Juan Sóto <juansheba97@gmail.com>
2022-10-12 14:43:44 +0200

    Mid day 12/10

--- src/components/ClientsSection.vue
+++ src/components/ClientsSection.vue
@@ -28,32 +28,41 @@
           >
         </a>
         <div class="flex items-center lg:order-2">
-          <PersonalRouter
-            href="#"
-            text="Login"
-            :route="route1"
-            class="
-              text-gray-800
-              dark:text-white
-              hover:bg-gray-50
-              focus:ring-4 focus:ring-gray-300
-              font-medium
-              rounded-lg
-              text-sm
-              px-4
-              lg:px-5
-              py-2
-              lg:py-2.5
-              mr-2
-              dark:hover:bg-gray-700
-              focus:outline-none
-              dark:focus:ring-gray-800
-            "
-          />
+          <button>
+            <svg
+              v-if="!darkLogo"
+              @click="toggleDark(), (darkLogo = !darkLogo)"
+              class="w-5 h-5"
+              fill="currentColor"
+              viewBox="0 0 20 20"
+              xmlns="http://www.w3.org/2000/svg"
+              color="white"
+            >
+              <path
+                d="M17.293 13.293A8 8 0 016.707 2.707a8.001 8.001 0 1010.586 10.586z"
+              ></path>
+            </svg>
+            <svg
+              v-if="darkLogo"
+              @click="toggleDark(), (darkLogo = !darkLogo)"
+              class="w-5 h-5"
+              fill="currentColor"
+              viewBox="0 0 20 20"
+              xmlns="http://www.w3.org/2000/svg"
+              color="black"
+            >
+              <path
+                d="M10 2a1 1 0 011 1v1a1 1 0 11-2 0V3a1 1 0 011-1zm4 8a4 4 0 11-8 0 4 4 0 018 0zm-.464 4.95l.707.707a1 1 0 001.414-1.414l-.707-.707a1 1 0 00-1.414 1.414zm2.12-10.607a1 1 0 010 1.414l-.706.707a1 1 0 11-1.414-1.414l.707-.707a1 1 0 011.414 0zM17 11a1 1 0 100-2h-1a1 1 0 100 2h1zm-7 4a1 1 0 011 1v1a1 1 0 11-2 0v-1a1 1 0 011-1zM5.05 6.464A1 1 0 106.465 5.05l-.708-.707a1 1 0 00-1.414 1.414l.707.707zm1.414 8.486l-.707.707a1 1 0 01-1.414-1.414l.707-.707a1 1 0 011.414 1.414zM4 11a1 1 0 100-2H3a1 1 0 000 2h1z"
+                fill-rule="evenodd"
+                clip-rule="evenodd"
+              ></path>
+            </svg>
+          </button>
+
           <PersonalRouter
             href=""
             text="Get Started"
-            :route="route"
+            :route="route1"
             class="
               text-white
               bg-blue-700
@@ -105,18 +114,6 @@
                 clip-rule="evenodd"
               ></path>
             </svg>
-            <svg
-              class="w-6 h-6"
-              fill="currentColor"
-              viewBox="0 0 20 20"
-              xmlns="http://www.w3.org/2000/svg"
-            >
-              <path
-                fill-rule="evenodd"
-                d="M4.293 4.293a1 1 0 011.414 0L10 8.586l4.293-4.293a1 1 0 111.414 1.414L11.414 10l4.293 4.293a1 1 0 01-1.414 1.414L10 11.414l-4.293 4.293a1 1 0 01-1.414-1.414L8.586 10 4.293 5.707a1 1 0 010-1.414z"
-                clip-rule="evenodd"
-              ></path>
-            </svg>
           </button>
         </div>
         <div
@@ -125,7 +122,8 @@
             justify-between
             items-center
             w-full
-            lg:flex lg:w-auto lg:order-1
+            lg:flex lg:w-auto lg:order
+            bg-gray
           "
           id="mobile-menu-2"
         >
@@ -147,7 +145,7 @@
                   pl-3
                   text-white
                   rounded
-                  bg-primary-700
+                  bg-blue-700
                   lg:bg-transparent lg:text-primary-700 lg:p-0
                   dark:text-white
                 "
